bdca42e1b7838dd496b18c03c9ceb19a848e26a1
d4wns-l1ght <neovx.23@proton.me>
2024-11-30 08:15:55 +1300

    Add fish functions from home computer

---
+++ fish/functions/barretts.fish
@@ -0,0 +1,4 @@
+function barretts --wraps='ssh wilkesluna@barretts.ecs.vuw.ac.nz' --description 'alias barretts=ssh wilkesluna@barretts.ecs.vuw.ac.nz'
+  ssh wilkesluna@barretts.ecs.vuw.ac.nz $argv
+        
+end
---
+++ fish/functions/greta-pt.fish
@@ -0,0 +1,4 @@
+function greta-pt --wraps='ssh wilkesluna@greta-pt.ecs.vuw.ac.nz' --description 'alias greta-pt=ssh wilkesluna@greta-pt.ecs.vuw.ac.nz'
+  ssh wilkesluna@greta-pt.ecs.vuw.ac.nz $argv
+        
+end
