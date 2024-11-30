6faadce5b64f4819d10d2b37109614f1c7847153
d4wns-l1ght <neovx.23@proton.me>
2024-11-30 14:04:33 +1300

    Add ~/.cargo/bin to path and start tracking fish/conf.d/

--- fish/config.fish
+++ fish/config.fish
@@ -1,7 +1,8 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
-set -gx PATH /opt/homebrew/bin:/opt/homebrew/bin:/usr/local/bin:/System/Cryptexes/App/usr/bin:/usr/bin:/bin:/usr/sbin:/sbin:/var/run/com.apple.security.cryptexd/codex.system/bootstrap/usr/local/bin:/var/run/com.apple.security.cryptexd/codex.system/bootstrap/usr/bin:/var/run/com.apple.security.cryptexd/codex.system/bootstrap/usr/appleinternal/bin
+set -gx PATH /opt/homebrew/bin:/opt/homebrew/bin:/usr/local/bin:$HOME/.cargo/bin:/System/Cryptexes/App/usr/bin:/usr/bin:/bin:/usr/sbin:/sbin:/var/run/com.apple.security.cryptexd/codex.system/bootstrap/usr/local/bin:/var/run/com.apple.security.cryptexd/codex.system/bootstrap/usr/bin:/var/run/com.apple.security.cryptexd/codex.system/bootstrap/usr/appleinternal/bin
+
 set -gx EDITOR nvim
 set -gx XDG_CONFIG_HOME ~/.config
 set -gx HOMEBREW_BUNDLE_FILE $XDG_CONFIG_HOME/homebrew/brewfile
