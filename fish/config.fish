a21a29b46e0dfe9f2316451ce936ec5a01c9086e
d4wns-l1ght <neovx.23@proton.me>
2025-04-13 14:16:42 +1200

    Switch to using fish_add_path

--- fish/config.fish
+++ fish/config.fish
@@ -1,13 +1,13 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
-set -gx PATH /opt/homebrew/bin:/opt/homebrew/bin:/usr/local/bin:/System/Cryptexes/App/usr/bin:/usr/bin:/bin:/usr/sbin:/sbin:/var/run/com.apple.security.cryptexd/codex.system/bootstrap/usr/local/bin:/var/run/com.apple.security.cryptexd/codex.system/bootstrap/usr/bin:/var/run/com.apple.security.cryptexd/codex.system/bootstrap/usr/appleinternal/bin:/Applications/Docker.app/Contents/Resources/bin
+fish_add_path /opt/homebrew/bin:/usr/local/bin:/~/.cargo/bin:/Applications/Docker.app/Contents/Resources/bin
 
 set -gx EDITOR nvim
 set -gx XDG_CONFIG_HOME ~/.config
 set -gx HOMEBREW_BUNDLE_FILE $XDG_CONFIG_HOME/homebrew/brewfile
 
-set -U tide_context_always_display true
+set -gx tide_context_always_display true
 
 source $XDG_CONFIG_HOME/fish/tide_catppuccin.fish
 source ~/.cargo/env.fish
