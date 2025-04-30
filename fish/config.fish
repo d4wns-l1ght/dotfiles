b848f9388e977fedf5530f4f63bcf3430d47c8ad
d4wns-l1ght <neovx.23@proton.me>
2025-04-30 13:32:11 +1200

    Add fish/local_settings.fish

--- fish/config.fish
+++ fish/config.fish
@@ -1,21 +1,14 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
-set -gx PATH $PATH /opt/homebrew/bin:/usr/local/bin:/Users/lunawilkes/.cargo/bin:/Applications/Docker.app/Contents/Resources/bin
+
+# set PATH, XDG_CONFIG_HOME, etc
+source ~/.config/fish/local_settings.fish
 
 set -gx EDITOR nvim
 set -gx MANPAGER "nvim +Man!"
-set -gx XDG_CONFIG_HOME ~/.config
-set -gx HOMEBREW_BUNDLE_FILE $XDG_CONFIG_HOME/homebrew/brewfile
 
 set -gx tide_context_always_display true
 
-source $XDG_CONFIG_HOME/fish/tide_catppuccin.fish
-source $XDG_CONFIG_HOME/fish/local_functions.fish
-source ~/.cargo/env.fish
-
-zoxide init fish | source
-
-
-# Created by `pipx` on 2025-04-17 08:36:59
-set PATH $PATH /Users/lunawilkes/.local/bin
+source ~/.config/fish/tide_catppuccin.fish
+source ~/.config/fish/local_functions.fish
