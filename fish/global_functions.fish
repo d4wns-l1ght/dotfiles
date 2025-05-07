4fca3eab158be73af35005c13a4c8b6a344a8121
d4wns-l1ght <neovx.23@proton.me>
2025-05-07 21:47:34 +1200

    Reorder global_functions a bit
    put the ugly git stuff at the bottom, order everything in order of
    complexity (simplest at the top)

--- fish/global_functions.fish
+++ fish/global_functions.fish
@@ -1,3 +1,7 @@
+# abbreviations
+abbr -a --position command ta tmux a -t
+
+# simple/wrapping functions
 function :q --description 'Exits the shell'
     exit
 end
@@ -6,20 +10,20 @@ function cat --wraps=bat --description 'alias cat=bat'
     bat $argv
 end
 
-function fish_greeting
-    if set -q SSH_CLIENT || set -q SSH_SHELL
-        pfetch
-    else if set -q TMUX || set -q ZELLIJ || set -q SCREEN
-        pfetch
-    else if command -v tmuxinator 2>&1 >/dev/null && test -f ~/.config/tmuxinator/home.yml
-        tmuxinator home
-    else
-        fastfetch
-    end
+function woman --wraps=man --description feminism
+    man $argv
 end
 
-function glog --wraps=git\ log\ --graph\ --abbrev-commit\ --decorate\ --format=format:\'\%C\(bold\ blue\)\%h\%C\(reset\)\ -\ \%C\(bold\ green\)\(\%ar\)\%C\(reset\)\ \%C\(white\)\%s\%C\(reset\)\ \%C\(dim\ white\)-\ \%an\%C\(reset\)\%C\(auto\)\%d\%C\(reset\)\'\ --all --description alias\ glog=git\ log\ --graph\ --abbrev-commit\ --decorate\ --format=format:\'\%C\(bold\ blue\)\%h\%C\(reset\)\ -\ \%C\(bold\ green\)\(\%ar\)\%C\(reset\)\ \%C\(white\)\%s\%C\(reset\)\ \%C\(dim\ white\)-\ \%an\%C\(reset\)\%C\(auto\)\%d\%C\(reset\)\'\ --all
-    git log --graph --abbrev-commit --decorate --format=format:'%C(bold blue)%h%C(reset) - %C(bold green)(%ar)%C(reset) %C(white)%s%C(reset) %C(dim white)- %an%C(reset)%C(auto)%d%C(reset)' --all $argv
+function pf 
+    clear; pfetch
+end
+
+function smux --description 'Creates or attaches to a tmux server over ssh' --wraps=ssh
+    ssh $argv -t 'tmux attach-session || tmux'
+end
+
+function svim --description 'Opens oil-ssh to a given address'
+    nvim oil-ssh://$argv
 end
 
 function mkcd
@@ -32,20 +36,20 @@ function mkz
     z "$argv"
 end
 
-function woman --wraps=man --description feminism
-    man $argv
+# fish related
+function fish_greeting
+    if set -q SSH_CLIENT || set -q SSH_SHELL
+        pfetch
+    else if set -q TMUX || set -q ZELLIJ || set -q SCREEN
+        pfetch
+    else if command -v tmuxinator 2>&1 >/dev/null && test -f ~/.config/tmuxinator/home.yml
+        tmuxinator home
+    else
+        fastfetch
+    end
 end
 
-function git_rebase_overwrite_author_committer --wraps=git\ -c\ rebase.instructionFormat=\'\%nexec\ GIT_COMITTER_DATE=\"\%cd\"\ GIT_AUTHOR_DATE=\"\%aD\"\ git\ commit\ --amend\ --no-edit\ --reset-author\ --allow-empty\'\ rebase\ -f
-    git -c rebase.instructionFormat='%s%nexec GIT_COMMITTER_DATE="%cD" GIT_AUTHOR_DATE="%aD" git commit --amend --no-edit --reset-author --allow-empty' rebase -f $argv
-end
-
-abbr -a --position command ta tmux a -t
-
-function pf 
-    clear; pfetch
-end
-
+# tmux related
 function switch_or_start_tmux_session
     set selected_session (begin
         tmux ls -F '#S'
@@ -65,11 +69,11 @@ function switch_or_start_tmux_session
     end
 end
 
-
-function smux --description 'Creates or attaches to a tmux server over ssh' --wraps=ssh
-    ssh $argv -t 'tmux attach-session || tmux'
+# git functions
+function glog --wraps=git\ log\ --graph\ --abbrev-commit\ --decorate\ --format=format:\'\%C\(bold\ blue\)\%h\%C\(reset\)\ -\ \%C\(bold\ green\)\(\%ar\)\%C\(reset\)\ \%C\(white\)\%s\%C\(reset\)\ \%C\(dim\ white\)-\ \%an\%C\(reset\)\%C\(auto\)\%d\%C\(reset\)\'\ --all --description alias\ glog=git\ log\ --graph\ --abbrev-commit\ --decorate\ --format=format:\'\%C\(bold\ blue\)\%h\%C\(reset\)\ -\ \%C\(bold\ green\)\(\%ar\)\%C\(reset\)\ \%C\(white\)\%s\%C\(reset\)\ \%C\(dim\ white\)-\ \%an\%C\(reset\)\%C\(auto\)\%d\%C\(reset\)\'\ --all
+    git log --graph --abbrev-commit --decorate --format=format:'%C(bold blue)%h%C(reset) - %C(bold green)(%ar)%C(reset) %C(white)%s%C(reset) %C(dim white)- %an%C(reset)%C(auto)%d%C(reset)' --all $argv
 end
 
-function svim --description 'Opens oil-ssh to a given address'
-    nvim oil-ssh://$argv
+function git_rebase_overwrite_author_committer --wraps=git\ -c\ rebase.instructionFormat=\'\%nexec\ GIT_COMITTER_DATE=\"\%cd\"\ GIT_AUTHOR_DATE=\"\%aD\"\ git\ commit\ --amend\ --no-edit\ --reset-author\ --allow-empty\'\ rebase\ -f
+    git -c rebase.instructionFormat='%s%nexec GIT_COMMITTER_DATE="%cD" GIT_AUTHOR_DATE="%aD" git commit --amend --no-edit --reset-author --allow-empty' rebase -f $argv
 end
